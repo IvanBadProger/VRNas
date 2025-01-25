9aef00685ba44cbc11c555254d1164c076c98b91
IvanBadProgger <programmerivan@mail.ru>
2025-01-26 00:44:02 +0700

    refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,38 @@
-const orbits = document.querySelectorAll('[data-solar-system=orbit]')
-const planets = document.querySelectorAll('[data-solar-system=planet]')
-const users = document.querySelectorAll('.testimoni__user')
+const selectors = {
+  orbits: document.querySelectorAll('[data-solar-system=orbit]'),
+  planets: document.querySelectorAll('[data-solar-system=planet]'),
+  users: document.querySelectorAll('.testimoni__user'),
+}
 
-orbits.forEach((item, index) => {
-  const baseWidth = 565
-  const step = 122
-  item.style.width = `${baseWidth + index * step}px`
-  item.style.height = `${baseWidth + index * step}px`
-})
+const orbits = document.querySelectorAll(selectors.orbits)
+const planets = document.querySelectorAll(selectors.planets)
+const users = document.querySelectorAll(selectors.users)
 
-const rotateAngle = 0.25
+const rotateAngle = 0.5
 
-anime({
+function createOrbits() {
+  orbits.forEach((item, index) => {
+    const baseWidth = 565
+    const step = 122
+    item.style.width = `${baseWidth + index * step}px`
+    item.style.height = `${baseWidth + index * step}px`
+  })
+}
+
+const animationPlanets = {
   targets: planets,
   rotate: (_, i) => `${i * rotateAngle + 1}turn`,
   easing: 'easeInOutQuad',
   duration: 3000,
-});
+}
 
-anime({
+const animationUsers = {
   targets: users,
   rotate: (_, i) => `${-i * rotateAngle}turn`,
   easing: 'easeInOutQuad',
   duration: 3000,
-})
+}
+
+createOrbits()
+anime(animationPlanets)
+anime(animationUsers)
