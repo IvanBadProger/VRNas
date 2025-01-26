c7c9c51cc294cbba5fcfdd74de2a149533975890
IvanBadProgger <programmerivan@mail.ru>
2025-01-26 14:55:35 +0700

    fix: adaptive bugs on click to testimonial-item and fix their location

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,65 @@
 const selectors = {
-  orbits: document.querySelectorAll('[data-solar-system=orbit]'),
-  planets: document.querySelectorAll('[data-solar-system=planet]'),
-  users: document.querySelectorAll('.testimoni__user'),
+  orbits: '[data-solar-system=orbit]',
+  planets: '[data-solar-system=planet]',
+  users: '.testimoni__user',
+  avatars: '.testimoni__avatar',
 }
 
 const orbits = document.querySelectorAll(selectors.orbits)
 const planets = document.querySelectorAll(selectors.planets)
 const users = document.querySelectorAll(selectors.users)
+const avatars = document.querySelectorAll(selectors.avatars)
 
-const rotateAngle = 0.5
+const rotateAngle = 0.35
 
-function createOrbits() {
+function setOrbitSize() {
   orbits.forEach((item, index) => {
-    const baseWidth = 565
-    const step = 122
+    const baseWidth = 550
+    const step = 150
     item.style.width = `${baseWidth + index * step}px`
     item.style.height = `${baseWidth + index * step}px`
   })
+}
+
+function showAvatars() {
+  avatars.forEach((item) => {
+    item.classList.add('testimoni__avatar--active')
+  })
+}
+
+function isElementOutOfViewportHorizontally(element) {
+  const buffer = 50
+  const menuWidth = 200
+  const rect = element.getBoundingClientRect()
+
+  return (
+    rect.right < buffer ||
+    rect.left > window.innerWidth - buffer - menuWidth
+  )
+}
+
+function checkPlanetsPosition() {
+  const step = 0.02
+
+  for (let index = 0; index < planets.length; index++) {
+    let rotatePlanet = parseFloat(
+      planets[index].style.transform.match(/-?\d*\.?\d+/)[0]
+    )
+
+    let rotateUser = parseFloat(
+      users[index].style.transform.match(/-?\d*\.?\d+/)[0]
+    )
+
+    while (isElementOutOfViewportHorizontally(users[index])) {
+      rotatePlanet += step
+      rotateUser -= step
+
+      planets[
+        index
+      ].style.transform = `rotate(${rotatePlanet}turn)`
+      users[index].style.transform = `rotate(${rotateUser}turn)`
+    }
+  }
 }
 
 const animationPlanets = {
@@ -24,6 +67,7 @@ const animationPlanets = {
   rotate: (_, i) => `${i * rotateAngle + 1}turn`,
   easing: 'easeInOutQuad',
   duration: 3000,
+  complete: () => {},
 }
 
 const animationUsers = {
@@ -31,8 +75,12 @@ const animationUsers = {
   rotate: (_, i) => `${-i * rotateAngle}turn`,
   easing: 'easeInOutQuad',
   duration: 3000,
+  complete: () => {
+    showAvatars()
+    checkPlanetsPosition()
+  },
 }
 
-createOrbits()
+setOrbitSize()
 anime(animationPlanets)
 anime(animationUsers)
