615d1b34f3e03cc74015e20c97d560c1500a3308
IvanBadProgger <programmerivan@mail.ru>
2025-01-26 15:26:07 +0700

    refactor

--- scripts/main.js
+++ scripts/main.js
@@ -5,26 +5,34 @@ const selectors = {
   avatars: '.testimoni__avatar',
 }
 
+const classes = {
+  avatarVisible: 'testimoni__avatar--visible',
+}
+
 const orbits = document.querySelectorAll(selectors.orbits)
 const planets = document.querySelectorAll(selectors.planets)
 const users = document.querySelectorAll(selectors.users)
 const avatars = document.querySelectorAll(selectors.avatars)
 
-const rotateAngle = 0.35
+const ROTATE_ANGLE = 0.35
+const ROTATE_STEP = 0.02
 
 function setOrbitSize() {
   orbits.forEach((item, index) => {
-    const baseWidth = 550
-    const step = 150
-    item.style.width = `${baseWidth + index * step}px`
-    item.style.height = `${baseWidth + index * step}px`
+    const orbitSizeBase = 550
+    const orbitSizeStep = 130
+    const sizeCurrentOrbit =
+      orbitSizeBase + index * orbitSizeStep
+
+    item.style.width = `${sizeCurrentOrbit}px`
+    item.style.height = `${sizeCurrentOrbit}px`
   })
 }
 
 function showAvatars() {
-  avatars.forEach((item) => {
-    item.classList.add('testimoni__avatar--active')
-  })
+  avatars.forEach((item) =>
+    item.classList.add(classes.avatarVisible)
+  )
 }
 
 function isElementOutOfViewportHorizontally(element) {
@@ -38,41 +46,42 @@ function isElementOutOfViewportHorizontally(element) {
   )
 }
 
-function checkPlanetsPosition() {
-  const step = 0.02
+function getRotateElement(element) {
+  const rotate = element.style.transform.match(/-?\d*\.?\d+/)
 
-  for (let index = 0; index < planets.length; index++) {
-    let rotatePlanet = parseFloat(
-      planets[index].style.transform.match(/-?\d*\.?\d+/)[0]
-    )
+  return rotate ? parseFloat(rotate[0]) : 0
+}
 
-    let rotateUser = parseFloat(
-      users[index].style.transform.match(/-?\d*\.?\d+/)[0]
-    )
+function updatePlanetAndUserPosition(index) {
+  let rotatePlanet = getRotateElement(planets[index])
+  let rotateUser = getRotateElement(users[index])
 
-    while (isElementOutOfViewportHorizontally(users[index])) {
-      rotatePlanet += step
-      rotateUser -= step
+  while (isElementOutOfViewportHorizontally(users[index])) {
+    rotatePlanet += ROTATE_STEP
+    rotateUser -= ROTATE_STEP
 
-      planets[
-        index
-      ].style.transform = `rotate(${rotatePlanet}turn)`
-      users[index].style.transform = `rotate(${rotateUser}turn)`
-    }
+    // prettier-ignore
+    planets[index].style.transform = `rotate(${rotatePlanet}turn)`;
+    users[index].style.transform = `rotate(${rotateUser}turn)`
   }
+}
+
+function checkPlanetsPosition() {
+  planets.forEach((_, index) =>
+    updatePlanetAndUserPosition(index)
+  )
 }
 
 const animationPlanets = {
   targets: planets,
-  rotate: (_, i) => `${i * rotateAngle + 1}turn`,
+  rotate: (_, i) => `${i * ROTATE_ANGLE + 1}turn`,
   easing: 'easeInOutQuad',
   duration: 3000,
-  complete: () => {},
 }
 
 const animationUsers = {
   targets: users,
-  rotate: (_, i) => `${-i * rotateAngle}turn`,
+  rotate: (_, i) => `${-i * ROTATE_ANGLE}turn`,
   easing: 'easeInOutQuad',
   duration: 3000,
   complete: () => {
@@ -81,6 +90,10 @@ const animationUsers = {
   },
 }
 
-setOrbitSize()
-anime(animationPlanets)
-anime(animationUsers)
+function initSolarSystem() {
+  setOrbitSize()
+  anime(animationPlanets)
+  anime(animationUsers)
+}
+
+initSolarSystem()
