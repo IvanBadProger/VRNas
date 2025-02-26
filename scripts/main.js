6084a50776cf3285fb758598cc484cf1e5cdd681
AryraAlex <mrzombi.com112@gmail.com>
2025-02-27 02:15:32 +0700

    refactor: section banner fill rename, refactor section hero and choose us, add hover animation button primary

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import { initSlider } from './slider.js'
 import { initSolarSystem } from './solar-system-animation.js'
 import { initCheckIcon } from './check-icons-animation.js'
 import { preloaderStart } from './preloader.js'
-import { showText} from './drop-down-text.js'
+import { initAccordions } from './drop-down-text.js'
 
 /**
  * Объект с элементами-триггерами, при видимости которых должны срабатывать соответствующие анимации
@@ -62,5 +62,5 @@ observer.observe(animationTriggers.checkIcon)
 observer.observe(animationTriggers.solarSystem)
 
 initSlider()
-showText()
+initAccordions()
 // preloaderStart()
